cf102ecf6347ad624dc048b9c34254adcec9d49a
Andrew Crites <explosion-pills@aysites.com>
2014-03-18 21:38:27 -0400

    first directive

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ app.factory('Messages', function () {
     return {message: "Service"};
 });
 
+app.directive("datepicker", function () {
+    return {
+        restrict: "E",
+        template: "<select><option>Month</option></select><select><option>Day</option></select>",
+    };
+});
+
 app.controller("MainCtrl", ["$scope", "Messages", function ($scope, Messages) {
     $scope.data = Messages;
     // Also update html `data.message` -> `message`
