2e203b290959f4e64aba52666f8db577862ae8f6
Andrew Crites <acrites@mobiquityinc.com>
2014-03-20 12:13:09 -0400

    isolate scope

--- main.js
+++ main.js
@@ -1,32 +1,11 @@
 var app = angular.module("frontend-awesome", []);
 
-app.factory('Messages', function () {
-    return {message: "Service"};
-});
-
-app.directive("counterHandler", function () {
-    return function (scope, element, attrs) {
-        element.bind("click", function () {
-            scope.$apply(attrs.counterHandler);
-        });
+app.directive("foo", function () {
+    return {
+        link: function (scope, elem, attrs) {
+            scope.foo = attrs.foo;
+        },
+        // scope: {},
+        template: "{{foo}}",
     };
 });
-
-app.controller("MainCtrl", ["$scope", "Messages", function ($scope, Messages) {
-    $scope.data = Messages;
-    // Also update html `data.message` -> `message`
-    // $scope.message = Messages.message;
-
-    $scope.changeMessage = function (data) {
-        data.message = "clicked";
-    };
-}]);
-
-app.controller("FooterCtrl", ["$scope", "$interval", "Messages", function ($scope, $interval, Messages) {
-    $scope.time = Date.now();
-    $scope.data = Messages;
-
-    $interval(function () {
-        $scope.time = Date.now();
-    }, 1000);
-}]);
